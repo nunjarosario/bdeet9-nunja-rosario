e04ad5d1f01a1a1f56247ca3c38c127d14baab26
nunjarosario <nunjarosario03@gmail.com>
2026-06-08 11:10:30 -0300

    .

--- 3) bases de datos.py
+++
@@ -1,8 +0,0 @@
-def todos_iguales(lista):
-    if len(lista)==0:
-        return True
-    primer_elemento= lista[0]
-    for elemento in lista:
-        if elemento!= primer_elemento:
-            return False
-        return True
